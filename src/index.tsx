ff283a201abcd12ecdccec3d56b898b6bd30a221
Andrew Chang-DeWitt <andrewchangdewitt@gmail.com>
2020-05-01 13:50:17 -0400

    moving state back to TodoList, no parent container needed

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,4 @@ import * as ReactDOM from 'react-dom'
 
 import { TodoList } from './components/TodoList'
 
-ReactDOM.render(
-  <TodoList compiler="Typescript" framework="React" />,
-  document.getElementById('example')
-)
+ReactDOM.render(<TodoList />, document.getElementById('example'))
